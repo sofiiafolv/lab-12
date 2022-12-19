7946f1f32700c55baa01b04f00847d4cad678c55
Sofiia Folvarochna <sofiyafolv@gmail.com>
2022-12-19 16:25:01 +0200

    small fixes

--- src/main/java/org/example/decorator/Main.java
+++ src/main/java/org/example/decorator/Main.java
@@ -3,7 +3,7 @@ package org.example.decorator;
 public class Main {
     public static void main(String[] args) {
         Document document = new SmartDocument("gs://oop-course/Geico-2021.png");
-        TimedDocument info = new TimedDocument(document);
-        System.out.println(info.getTime());
+        document = new TimedDocument(document);
+        System.out.println(document.parse());
     }
 }
--- src/main/java/org/example/decorator/TimedDocument.java
+++ src/main/java/org/example/decorator/TimedDocument.java
@@ -4,15 +4,19 @@ import lombok.Getter;
 
 import java.util.concurrent.TimeUnit;
 
-public class TimedDocument{
+public class TimedDocument implements Document{
     private final Document document;
-    @Getter
-    private final double time;
     TimedDocument(Document document){
         this.document = document;
+
+    }
+
+    @Override
+    public String parse() {
         long start = System.nanoTime();
         this.document.parse();
         long elapsedTime = System.nanoTime() - start;
-        time = (double)elapsedTime / 1_000_000_000.0;
+        System.out.println((double)elapsedTime / 1_000_000_000.0);
+        return document.parse();
     }
 }
